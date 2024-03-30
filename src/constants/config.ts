308511566815f90a0b3f64a870e04e2ef129c405
Thadhermeg <165406919+Thadhermeg@users.noreply.github.com>
2024-03-30 11:49:10 +0100

    config

--- src/constants/config.ts
+++ src/constants/config.ts
@@ -11,7 +11,7 @@ export const CONFIG = {
     //
     googleAnalytics: "", // You can use this if you use Google Analytics
     shuffle: true, // whether to shuffle the words in the wordlist each time you load the app (note: you will lose the 'word of the day' functionality if this is true)
-    normalization: 'NFC', // whether to apply Unicode normalization to words and orthography - options: 'NFC', 'NFD', 'NKFC', 'NKFD', false
+    normalization: 'NFD', // whether to apply Unicode normalization to words and orthography - options: 'NFC', 'NFD', 'NKFC', 'NKFD', false
     startDate: 'March 30, 2024 00:00:00', // what date and time to start your game from
     defaultLang: 'en', // the default interface language
     availableLangs: ['en', 'es'], // the options available to the user for translation languages
